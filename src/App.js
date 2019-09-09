1e080de7e95e6f65e47c3a98f79a17b4774e8d60
cande04 <cory.anders.atc@gmail.com>
2019-09-09 13:26:40 -0400

    spoonacular api set up

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import React, { Component } from 'react'
 import './App.scss'
 import { Route } from 'react-router-dom'
 
-import AuthenticatedRoute from './auth/components/AuthenticatedRoute'
 import Header from './header/Header'
-import SignUp from './auth/components/SignUp'
-import SignIn from './auth/components/SignIn'
-import SignOut from './auth/components/SignOut'
-import ChangePassword from './auth/components/ChangePassword'
+import Wines from './wine/Wines'
 
 import Alert from 'react-bootstrap/Alert'
 
@@ -16,14 +12,9 @@ class App extends Component {
     super()
 
     this.state = {
-      user: null,
       alerts: []
     }
   }
-
-  setUser = user => this.setState({ user })
-
-  clearUser = () => this.setState({ user: null })
 
   alert = (message, type) => {
     this.setState({ alerts: [...this.state.alerts, { message, type }] })
@@ -43,17 +34,8 @@ class App extends Component {
           </Alert>
         ))}
         <main className="container">
-          <Route path='/sign-up' render={() => (
-            <SignUp alert={this.alert} setUser={this.setUser} />
-          )} />
-          <Route path='/sign-in' render={() => (
-            <SignIn alert={this.alert} setUser={this.setUser} />
-          )} />
-          <AuthenticatedRoute user={user} path='/sign-out' render={() => (
-            <SignOut alert={this.alert} clearUser={this.clearUser} user={user} />
-          )} />
-          <AuthenticatedRoute user={user} path='/change-password' render={() => (
-            <ChangePassword alert={this.alert} user={user} />
+          <Route path='/wine-pairing-list' render={() => (
+            <Wines alert={this.alert} />
           )} />
         </main>
       </React.Fragment>
---
+++ src/wine/Wines.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class Wines extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      wines: [],
+      pairingText: null,
+      loaded: false
+    }
+  }
+
+  componentDidMount () {
+    axios('https://api.spoonacular.com/food/wine/pairing?food=steak&apiKey=6447eeed093f4db6a0690426c5319196')
+      .then(res => this.setState({ wines: res.data.pairedWines, pairingText: res.data.pairingText, loaded: true }))
+      .catch(console.err)
+  }
+
+  render () {
+    const { wines, pairingText, loaded } = this.state
+    const wineList = wines.map((wine, index) => (
+      <li key={index}>
+        {wine}
+      </li>
+    ))
+
+    if (!loaded) {
+      return <h3>...loading...</h3>
+    }
+
+    if (wines.length === 0) {
+      return <p>could not find a wine pairing!</p>
+    }
+
+    return (
+      <div>
+        <h2>{pairingText}</h2>
+        <ul>
+          {wineList}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Wines
