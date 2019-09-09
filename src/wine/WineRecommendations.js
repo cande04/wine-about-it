22e02bb23deac861ef339d81b7f7584f16742933
cande04 <cory.anders.atc@gmail.com>
2019-09-09 15:43:14 -0400

    create initial wine recs page

--- src/wine/WineRecommendations.js
+++ src/wine/WineRecommendations.js
@@ -10,17 +10,13 @@ import CardActions from '@material-ui/core/CardActions'
 import Collapse from '@material-ui/core/Collapse'
 import IconButton from '@material-ui/core/IconButton'
 import Typography from '@material-ui/core/Typography'
-import { red } from '@material-ui/core/colors'
-import FavoriteIcon from '@material-ui/icons/Favorite'
-import ShareIcon from '@material-ui/icons/Share'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
-import MoreVertIcon from '@material-ui/icons/MoreVert'
 
 import Paper from '@material-ui/core/Paper'
 
 const styles = {
   card: {
-    maxWidth: 345
+    maxWidth: '60%'
   },
   media: {
     height: 0,
@@ -32,9 +28,6 @@ const styles = {
   },
   expandOpen: {
     transform: 'rotate(180deg)'
-  },
-  avatar: {
-    backgroundColor: red[500]
   }
 }
 
@@ -51,47 +44,40 @@ class WineRecommendations extends Component {
 
   componentDidMount () {
     axios(`https://api.spoonacular.com/food/wine/recommendation?wine=${this.props.match.params.wine}&number=10&apiKey=6447eeed093f4db6a0690426c5319196`)
-      .then(res => this.setState({ recommendations: res.data.recommededWines }))
+      .then(res => {
+        console.log(res.data.recommendedWines)
+        this.setState({ recommendations: res.data.recommendedWines, wineType: this.props.match.params.wine })
+      })
+      .catch(console.err)
   }
 
-  handleExpandClick () {
-    this.setState(!this.state.expanded)
+  handleExpandClick = (event) => {
+    event.preventDefault()
+    this.setState({ expanded: !this.state.expanded })
   }
 
   render () {
     return (
       <div>
         <Paper>
+          <h2>{this.state.wineType} recommendations</h2>
           {this.state.recommendations.map((wine, index) =>
             <div key={index}>
-              <Card className={styles.card}>
+              <Card style={styles.card}>
                 <CardHeader
-                  action={
-                    <IconButton aria-label="settings">
-                      <MoreVertIcon />
-                    </IconButton>
-                  }
-                  title="Shrimp and Chorizo Paella"
-                  subheader="September 14, 2016"
+                  title={wine.title}
                 />
                 <CardMedia
-                  className={styles.media}
-                  image="/static/images/cards/paella.jpg"
-                  title="Paella dish"
+                  style={styles.media}
+                  image={wine.imageUrl}
+                  title="wine image"
                 />
                 <CardContent>
                   <Typography variant="body2" color="textSecondary" component="p">
-                    This impressive paella is a perfect party dish and a fun meal to cook together with your
-                    guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                    Learn More
                   </Typography>
                 </CardContent>
                 <CardActions disableSpacing>
-                  <IconButton aria-label="add to favorites">
-                    <FavoriteIcon />
-                  </IconButton>
-                  <IconButton aria-label="share">
-                    <ShareIcon />
-                  </IconButton>
                   <IconButton
                     className={clsx(styles.expand, {
                       [styles.expandOpen]: this.state.expanded
@@ -105,28 +91,25 @@ class WineRecommendations extends Component {
                 </CardActions>
                 <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
                   <CardContent>
-                    <Typography paragraph>Method:</Typography>
+                    <Typography paragraph>Description:</Typography>
                     <Typography paragraph>
-                      Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                      minutes.
+                      {wine.description}
+                    </Typography>
+                    <Typography paragraph>Average Score:</Typography>
+                    <Typography paragraph>
+                      {wine.score.toString().slice(0, 4)}
                     </Typography>
                     <Typography paragraph>
-                      Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                      heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-                      browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-                      and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                      pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-                      saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+                      Price:
                     </Typography>
                     <Typography paragraph>
-                      Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-                      without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-                      medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-                      again without stirring, until mussels have opened and rice is just tender, 5 to 7
-                      minutes more. (Discard any mussels that don’t open.)
+                      {wine.price}
                     </Typography>
-                    <Typography>
-                      Set aside off of the heat to let rest for 10 minutes, and then serve.
+                    <Typography paragraph>
+                      Buy me!
+                    </Typography>
+                    <Typography paragraph>
+                      {wine.link}
                     </Typography>
                   </CardContent>
                 </Collapse>
